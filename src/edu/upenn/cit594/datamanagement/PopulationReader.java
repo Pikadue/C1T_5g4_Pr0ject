f080479544ace5b58f7a2f7d65e5e72308035dab
Pikapika <pikadue@gmail.com>
2019-11-30 12:21:21 -0500

    first commit

---
+++ src/edu/upenn/cit594/datamanagement/PopulationReader.java
@@ -0,0 +1,31 @@
+package edu.upenn.cit594.datamanagement;
+
+import edu.upenn.cit594.data.Residence;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PopulationReader {
+    public HashMap<Integer, Integer> read(String fileName) throws FileNotFoundException {
+        Map<Integer, Integer> populationMap = new HashMap<>();//TODO
+        File newFile = new File(fileName);
+        BufferedReader br = new BufferedReader(new FileReader(newFile));
+        String line;
+        try{
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+
+
+            }
+        } catch(IOException e){
+
+
+        }
+
+        return null;
+
+    }
+}
