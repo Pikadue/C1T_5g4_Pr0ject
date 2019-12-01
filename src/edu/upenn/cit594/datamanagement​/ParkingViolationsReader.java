acc8ccb2ff85cbb74f6e0d9c6665e459338be203
Han Xiao <hannyc@seas.upenn.edu>
2019-11-30 19:40:57 -0500

    manually delete duplicate folder

--- src/edu/upenn/cit594/datamanagement​/ParkingViolationsReader.java
+++
@@ -1,9 +0,0 @@
-package edu.upenn.cit594.datamanagement​;
-
-
-import java.util.List;
-import edu.upenn.cit594.data.*;
-
-public interface ParkingViolationsReader {
-	public List<ParkingViolation> getAllParkingViolation();
-}
