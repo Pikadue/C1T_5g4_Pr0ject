f19e0d714f9209eef6e8ee1307c2752ccfebea74
Pikapika <pikadue@gmail.com>
2019-11-30 19:59:16 -0500

    Add main, try userInput class

--- src/edu/upenn/cit594/ui/userInput.java
+++ src/edu/upenn/cit594/ui/userInput.java
@@ -19,6 +19,32 @@ public class userInput {
         Scanner in  = new Scanner(System.in);
         String userInput = in.nextLine();
         if (isCorrect(userInput)) {
+            switch(userInput) {
+                case "0":
+                    System.exit(0);
+                    break;
+                case "1":
+                    System.out.println("Select 1");
+                    break;
+                case "2":
+                    System.out.println("Select 2");
+                    break;
+                case "3":
+                    System.out.println("Select 3");
+                    break;
+                case "4":
+                    System.out.println("Select 4");
+                    break;
+                case "5":
+                    System.out.println("Select 5");
+                    break;
+                case "6":
+                    System.out.println("Select 6");
+                    break;
+                default:
+                    System.out.println("Invalid selection");
+                    System.exit(0);
+            }
 
         }
     }
